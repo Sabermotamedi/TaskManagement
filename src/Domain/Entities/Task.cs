ffa90040703f465e7c18f84db6b5cafbcaaad9a8
s.motamedi <s.motamedi@rayanbourse.ir>
2023-05-30 22:59:49 +0330

    Initial project

---
+++ src/Domain/Entities/Task.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Task : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskType Type { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public StatusState Status { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
